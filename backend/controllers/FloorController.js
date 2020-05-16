3efef1a7466379ed3dc2b588df5d1d57f37e292d
jadson179 <jadson44.santos@gmail.com>
2020-05-16 17:26:08 -0300

    Provisionamento automático do layout

--- backend/controllers/FloorController.js
+++ backend/controllers/FloorController.js
@@ -1,54 +1,57 @@
 const database = require('../database/connection')
 
 module.exports = {
-    async index (request,response) {       
+    async index(request, response) {
 
         const results = await database('Floors')
-        .select('*')
-        .join('Rooms', 'Floors.RoomID', 'Rooms.RoomID')
-        .orderBy('Rooms.NumberRoom','asc')
-       
-        return response.json(results)
+            .select('*')
+            .join('Rooms', 'Floors.RoomID', 'Rooms.RoomID')
+            .orderBy('Rooms.NumberRoom', 'asc')
+
+        const query = await database('Floors')
+            .distinct('NumberFloor')
+
+        return response.json({ results, query })
 
     },
-    async store (request, response) {
+    async store(request, response) {
 
-        const [{qtdfloors}] = await database('Floors')
-        .count('*',{as: 'qtdfloors'})
-        .where('NumberFloor','=', request.body.numberfloor)
+        const [{ qtdfloors }] = await database('Floors')
+            .count('*', { as: 'qtdfloors' })
+            .where('NumberFloor', '=', request.body.numberfloor)
 
         console.log(qtdfloors)
 
-        if(qtdfloors == 0){
+        if (qtdfloors == 0) {
             await database('Floors')
-            .insert({
-                NumberFloor: request.body.numberfloor,
-                RoomID: request.body.roomid
-            })
+                .insert({
+                    NumberFloor: request.body.numberfloor,
+                    RoomID: request.body.roomid
+                })
 
-            return response.json({status: "Success in create floor"})
-        }else{
-            return response.json({status: "Faild in create floor"})
+            return response.json({ status: "Success in create floor" })
+        } else {
+            return response.json({ status: "Faild in create floor" })
         }
 
     },
-    async update (request, response) {   
+    async update(request, response) {
 
         const results = await database('Floors')
-        .update({
-            NumberFloor:request.body.newnumberfloor,
-        })
-        .where('NumberFloor','=',request.body.numberfloor)
-        
+            .update({
+                NumberFloor: request.body.newnumberfloor,
+            })
+            .where('NumberFloor', '=', request.body.numberfloor)
+
         return response.json(results)
 
     },
-    async delete (request, response){
-       
+    async delete(request, response) {
+
         const results = await database('Floors')
-        .where('NumberFloor','=',request.body.numberfloor)
-        .del()
-        
+            .where('NumberFloor', '=', request.body.numberfloor)
+            .del()
+
         return response.json(results)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ const app = express()
 const server = http.Server(app)
 const io = require('socket.io')(server)
 
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
