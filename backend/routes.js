cbcfdf601a3f66400ffca579dec6cb486906de7b
jadson179 <jadson44.santos@gmail.com>
2020-06-16 13:39:10 -0300

    ajustando api url yahoo

--- backend/routes.js
+++ backend/routes.js
@@ -67,7 +67,7 @@ routes.get('/forencast', function (req, res) {
     );
 
     request.get(
-        'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json',
+        'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=florianópolis&u=c&format=json',
         null,
         null,
         function (err, data, result) {
