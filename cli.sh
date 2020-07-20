67ef734b20a05f7caf731757f37c6682c88f8c17
jadson179 <jadson44.santos@gmail.com>
2020-07-20 18:53:52 -0300

    Inserido header to cli

--- cli.sh
+++ cli.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# MIT License
+# 
+# Copyright (c) 2020 Arthur Ribeiro
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 install() {
     docker-compose up --build painel
 }
@@ -12,7 +34,7 @@ update(){
         docker cp ./backup/database.db $(docker container ls -q):/backend/database
         echo "database ok"
     else
-        echo "file database.db is not exists"
+        echo "file database.db not exists"
     fi    
 
       if [ -d ./backup/imgs ]; 
@@ -20,7 +42,7 @@ update(){
         docker cp ./backup/imgs $(docker container ls -q):/backend
         echo "imgs ok"
     else
-        echo "The folder imgs is not exists"
+        echo "The folder imgs not exists"
     fi  
 }
 
